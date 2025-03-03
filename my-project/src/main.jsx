0d342cc4ff618bfc11707f7b7c8bc91430141f06
Pakasin <teen.tenparkasin@gmail.com>
2025-03-03 19:19:44 +0700

    rename main

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -5,7 +5,7 @@ import CustomNavbar from './Navbar';
 import Login1 from './Login1';
 import Home1 from './Home1';
 import Signup1 from './Signup1';
-import Crat from './Crat';
+import Cart from './Cart';
 import OrderSummary from './OrderSummary';
 import ProtectedRoute from './ProtectedRoute';
 import './login1.css';
@@ -21,7 +21,7 @@ root.render(
                 <Route path="/home" element={<Home1 />} />
                 <Route path="/signup" element={<Signup1 />} />
                 <Route element={<ProtectedRoute />}>
-                    <Route path="/crat" element={<Crat />} />
+                    <Route path="/cart" element={<Cart />} />
                     <Route path="/order-summary" element={<OrderSummary />} />
                 </Route>
             </Routes>
